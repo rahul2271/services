bc7fa0a511d6a02c853ae710c6da0a71f65425dd
Rahul <121254290+rahul2271@users.noreply.github.com>
2025-01-29 10:36:27 +0530

    Update kit.js

--- components/kit.js
+++ components/kit.js
@@ -133,7 +133,7 @@ const ProductPage = () => {
       </div>
 
       <motion.div
-        id="form-section"
+        
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
